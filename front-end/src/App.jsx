26d8bfaa937826e41f1a6f5ede2720e1fe3a7923
JoaoManoelFontes <jmanufontesc@gmail.com>
2022-05-10 16:55:21 -0300

    initial front-end in react commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import { Form } from './components/form';
+
+function App() {
+  
+  return (
+      <main className="main">
+        <div className='title'>
+          <h1>ELEIÇÕES</h1>
+          <h1>NADIC</h1>
+        </div>
+        <div>
+          <Form />
+        </div>
+      </main>
+  )
+}
+
+export default App
